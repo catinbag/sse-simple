5834fa1d9c902a09dbb1baf3bc3b88cdcbf08d21
Силин Дмитрий <true.litry@gmail.com>
2022-12-03 13:40:52 +0300

    удалены таймеры

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const EventEmitter = require("events").EventEmitter;
 
 const PORT = 8080;
 
@@ -8,44 +9,59 @@ const sseRoute = "/stream";
 
 let data;
 
-function sse(req, res) {
-  res.setHeader("Content-Type", "text/event-stream");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
+class SSE extends EventEmitter {
+  constructor() {
+    super();
+  }
 
-  let id = 0;
+  init(req, res) {
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
 
-  const timer = setInterval(() => {
-    if (data !== undefined && data !== "") {
-      res.write(`data: ${data}\n`);
-      res.write(`id: ${id++}\n`);
-      res.write(`\n`);
+    this.req = req;
+    this.res = res;
+
+    let id = 0;
+
+    function dataListener(data) {
+      if (data.event !== undefined) {
+        res.write(`event: ${data.event}\n`);
+      }
+
+      this.res.write(`data: ${data.data}\n`);
+      this.res.write(`id: ${id++}\n`);
+      this.res.write(`\n`);
     }
-  }, 2_000);
 
-  // setTimeout(() => {
-  //   clearInterval(timer);
-  //   res.write(`event: end-of-stream\n`);
-  //   res.write(`data: end\n`);
-  //   res.write(`\n`);
+    this.on("data", dataListener);
 
-  //   res.end("ok");
-  // }, 5_000);
+    req.on("close", () => {
+      this.removeListener("data", dataListener);
+    });
+  }
+
+  send(data) {
+    this.emit("data", data);
+  }
 }
+
+const sse = new SSE();
 
 http
   .createServer((req, res) => {
     const url = new URL(`http://${req.headers.host}${req.url}`);
 
     if (url.pathname === sseRoute) {
-      sse(req, res);
+      sse.init(req, res);
 
       return;
     }
 
     if (url.pathname === "/send-msg") {
       data = url.searchParams.get("message");
-      res.end(data);
+      sse.send({ data });
+      res.end("ok");
 
       return;
     }
